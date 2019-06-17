5c41db7444e77a4a03862e51f57fd0f3639bc920
Roberto D. Garcia <robertdavid_mgt@hotmail.es>
2019-06-16 19:40:32 -0600

    Método Update

--- models/motivo.php
+++ models/motivo.php
@@ -39,6 +39,7 @@
          * @return boolean
          */
         public function save() {
+            $go = false;
             try {
                 // prepare statement for insert
                 $sql = " INSERT INTO " . $this->table_name . "(motivo, des_motivo, estado, tipo) VALUES (:motivo, :des_motivo, :estado, :tipo)";
@@ -58,16 +59,44 @@
                 
                 // execute the insert statement
                 if($stmt->execute()){
-                    return true;
+                    $go = true;
                 }
-                return false;
             } catch (\PDOException $e) {
-                print_r($e);
-                throw new \Exception("Error reading database configuration file");
-                //return false;
+                //print_r($e);
+                //throw new \Exception("DataBase Error!");
             }
+
+            return $go;
         }
 
+         /**
+         * Update stock based on the specified id
+         * @return int
+         */
+        public function update() {
+    
+            // sql statement to update a row in the stock table
+            $sql = 'UPDATE '. $this->table_name
+                    . ' SET '
+                    . 'des_motivo = :des_motivo, '
+                    . 'estado = :estado, '
+                    . 'tipo = :tipo, '
+                    . 'WHERE motivo = :motivo';
+    
+            $stmt = $this->pdo->prepare($sql);
+    
+            // bind values to the statement
+            $stmt->bindValue(':des_motivo', $this->des_motivo);
+            $stmt->bindValue(':estado', $this->estado);
+            $stmt->bindValue(':tipo', $this->tipo);
+            $stmt->bindValue(':motivo', $this->motivo);
+            
+            // update data in the database
+            $stmt->execute();
+    
+            // return the number of row affected
+            return $stmt->rowCount();
+        }
 
     }
 ?>
