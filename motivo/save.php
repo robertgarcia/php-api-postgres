ba0a2d55bb103c3d2755c3dcfb004f62650b2c79
Roberto D. Garcia <robertdavid_mgt@hotmail.es>
2019-06-16 19:40:58 -0600

    Fix método Save

--- motivo/save.php
+++ motivo/save.php
@@ -40,8 +40,8 @@
             http_response_code(201);
     
             // tell the user
-            echo json_encode(array("message" => "Product was created."));
-        }else{ // if unable to create the product, tell the user
+            echo json_encode(array("message" => "Row was created."));
+        }else{ // if unable to create, tell the user
     
             // set response code - 503 service unavailable
             http_response_code(503);
